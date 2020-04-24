987cc569896efae6d5d3d29e07420ddca8ab2cbc
Oskar Wennström <54888925+wennstrom@users.noreply.github.com>
2020-04-24 17:30:59 +0200

    Add files via upload

---
+++ Classes/Sphere.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    public class Sphere : Box
+    {
+        readonly int id;
+        readonly string description;
+        readonly double weight;
+        readonly int side;
+        readonly int radius;
+        readonly bool fragile;
+        public Sphere(int id, string description, double weight, int radius, bool fragile)
+        {
+            ID = id;
+            Description = description;
+            Weight = weight;
+            Radius = radius;
+            Maxdimension = radius + radius;
+            IsFragile = fragile;
+
+            Area = Convert.ToInt32(Math.Round(4 * Math.PI * (radius * radius)));    //räknar ut arean och konventerar double till int
+            Volume = Convert.ToInt32(Math.Round((4 / 3) * Math.PI * (radius * radius * radius)));
+
+        }
+        public int Radius { get; set; }
+    }
+}
---
+++ Classes/WareHouseLocation.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    public class WareHouseLocation
+    {
+        public readonly List<Box> Boxes = new List<Box>();
+        public List<Box> boxes
+        {
+            get { return Boxes; }
+        }
+        
+        public WareHouseLocation()
+        {
+
+            MaxVolume = Height * Width * Debth;
+        }
+        public int Height { get;  } = 250;
+        public int Width { get;  } = 250;
+        public int Debth { get;  } = 250;
+        public double MaxWeight { get;  } = 1000;
+        public double MaxVolume { get; }
+        public WareHouseLocation Content()
+        {
+            return (WareHouseLocation)this.MemberwiseClone();
+        }
+        public Box GetBox(int id)
+        {
+            foreach (var box in Boxes)
+            {
+                if (box.ID == id)
+                    return box;
+            }
+
+            return null;
+        }
+        public void AddBox(Box b)
+        {
+            if (Addable(b))
+            {
+                boxes.Add(b);
+            }
+            else
+            {
+                Console.WriteLine("The box could not be ");
+            }
+
+
+            
+        }
+        
+        public bool Addable(Box b)
+        {
+            double newWeight = CalculateCurrentWeight() + b.Weight;
+            double newVolume = CalculateCurrentVolume() + b.Volume;
+
+            if (newWeight < MaxWeight && MaxVolume > newVolume)
+                return true;
+            else
+                return false;
+        }
+        double CalculateCurrentWeight()
+        {
+            double weight = 0;
+
+            foreach (var b in Boxes)
+                weight += b.Weight;
+
+            return weight; 
+            
+        }
+
+        double CalculateCurrentVolume()
+        {
+            double volume = 0;
+
+            foreach (var b in Boxes)
+                volume += b.Volume;
+
+            return volume;
+
+        }
+    }
+}
